cb9ae3d4841647e5bfac3ef03eeff36ec3ee8dc7
icloud <yinaiyun@live.cn>
2018-02-19 23:18:59 +0800

    add layout

---
+++ src/main.js
@@ -0,0 +1,20 @@
+function initMap() {
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+}
+
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        console.log('-----')
+        $('#sidebar').toggleClass('active');
+    });
+
+});
