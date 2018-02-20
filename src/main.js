19987c827a8fed414d2a365e7dc369100f098127
icloud <yinaiyun@live.cn>
2018-02-20 11:28:34 +0800

    add knockout,and locations

--- src/main.js
+++ src/main.js
@@ -1,20 +1,86 @@
+
+var map;
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 34.268025, lng: 108.947175},
+        zoom: 12,
+        disableDefaultUI: true
     });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
+    locations.forEach(function(location){
+        new google.maps.Marker({
+            position: new google.maps.LatLng(location.lat, location.lng),
+            map: map,
+            animation: google.maps.Animation.DROP
+        });
     });
-}
+};
 
 $(document).ready(function () {
-
     $('#sidebarCollapse').on('click', function () {
-        console.log('-----')
         $('#sidebar').toggleClass('active');
     });
-
 });
+
+// 
+var locations = [
+    {
+        name: "大唐芙蓉园",
+        lat: 34.211048,
+        lng: 108.9749846
+    },
+    {
+        name: "大雁塔",
+        lat: 34.2171601,
+        lng: 108.9662626,
+    },
+    {
+        name: "陕西历史博物馆",
+        lat: 34.220508,
+        lng: 108.9559011,
+    },
+    {
+        
+        name: "钟楼",
+        lat: 34.2583144,
+        lng: 108.9451903
+    },
+    {
+        name: "碑林博物馆",
+        lat: 34.2520922,
+        lng: 108.9277647
+    }
+];
+
+// Place Object.
+
+var Place = function(place){
+    "use strict";
+    this.name = ko.observable(place.name);
+    this.lat = ko.observable(place.lat);
+    this.lng = ko.observable(place.lng);
+}
+
+!function(){
+    var LocationsViewMode = function() {
+        var self = this;
+        self.placeList = ko.observableArray([]);
+
+        locations.forEach(function(p){
+            self.placeList.push(new Place(p));
+        });
+
+        self.onSelect = function(address) {
+            console.log('Your select:', address);
+        };
+
+        // self.placeList().forEach(function(place){
+        //     new google.maps.Marker({
+        //         position: new google.maps.LatLng(place.lat(), place.lng()),
+        //         map: map,
+        //         animation: google.maps.Animation.DROP
+        //     });
+        // });
+    };
+
+    ko.applyBindings(new LocationsViewMode());
+} ();
